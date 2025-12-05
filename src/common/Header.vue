332f2777addedb1cd7b22d976c12e37e578a5024
rsvdrigo <rsvdrigo@gmail.com>
2025-12-05 19:54:39 -0300

    chore: add folder style and global configs

--- src/common/Header.vue
+++ src/common/Header.vue
@@ -1,23 +1,40 @@
 <script setup>
 import { Icon } from '@iconify/vue';
+import logoInspire from "../assets/logoInspire.png"
+
+
 </script>
 
 <template>
-<header>
-    <div class></div>
-    <div>
-        <nav>
-            <Icon class="search-icon" icon="material-symbols:search" width="40"/>
-            <a href="">Início</a>
-            <a href="">Galeria</a>
-            <a href="">Favoritos</a>
-            <Icon class="user-icon" icon="material-symbols:person" width="40"/>
-            
-        </nav>
-    </div>
-</header>
+    <header>
+        <img :src="logoInspire" alt="Imagem do Logo da inspire pixel">
+        <div class="navbar-manu">
+            <nav>
+                <Icon class="search-icon nav-icon" icon="material-symbols:search" width="40" />
+                <a href="#">Início</a>
+                <a href="#">Galeria</a>
+                <a href="#">Favoritos</a>
+                <Icon class="user-icon nav-icon" icon="material-symbols:person" width="40" />
+
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style>
+<style scoped lang="scss">
+@use "@/style/_variables.scss" as vars;
+
+
+header{
+    display:flex;
+    justify-content: space-between;
+    padding: 4.2vh 7.6vh 4.2vh 7.7vh;  
+}
+nav{
+    display: flex;
+    align-items: center;
+    gap: 6.4vh;
+    font-family: vars.$font-poppins;
+}
 
 </style>
