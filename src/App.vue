8ed567f586ad8abddfdd44b566133391ff8f0a06
rsvdrigo <rsvdrigo@gmail.com>
2025-12-05 18:40:00 -0300

    feat: add Header.vue and import to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <script setup>
-
+import Header from './common/Header.vue';
 </script>
+
+<template>
+<Header/>
+</template>
+
+<style>
+
+</style>
---
+++ src/common/Header.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<template>
+<header>
+    <div class></div>
+    <div>
+        <nav>
+            <Icon class="search-icon" icon="material-symbols:search" width="40"/>
+            <a href="">Início</a>
+            <a href="">Galeria</a>
+            <a href="">Favoritos</a>
+            <Icon class="user-icon" icon="material-symbols:person" width="40"/>
+            
+        </nav>
+    </div>
+</header>
+</template>
+
+<style>
+
+</style>
