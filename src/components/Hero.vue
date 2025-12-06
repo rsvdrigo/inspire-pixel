7e45fae51912b9b01013c9f46691aabba44eb3fc
rsvdrigo <rsvdrigo@gmail.com>
2025-12-06 13:40:53 -0300

    feat: add html structure to Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,12 @@
 <script setup>
+import Fotografo from "../assets/fotografo.png"
 </script>
 <template>
-    
+    <div class="hero-text">
+    <h1>Onde a paisagem vira <span>inspiração</span></h1>
+    <h3>Explore paisagens, cores e luzes que inspiram novas histórias através da fotografia.</h3>
+    </div>
+    <img :src="Fotografo" alt="Imagem de um(a) fotográfogo.">
 
 </template>
 <style scoped lang="scss"></style>
